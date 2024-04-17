ec003efa3d5a0b3ec48217e1fe7559b50b3590fe
Danilo Marano <danilo.m.marano@gmail.com>
2024-04-17 10:24:57 -0300

    :sparkles: feat: Create next app with Shadcn/ui

---
+++ lib/theme.ts
@@ -0,0 +1,11 @@
+// Functions to configure the page color scheme
+
+export function setDark() {
+    document.documentElement.classList.add('dark');
+    document.documentElement.classList.remove('light');
+}
+
+export function setLight() {
+    document.documentElement.classList.add('dark');
+    document.documentElement.classList.remove('light');
+}
